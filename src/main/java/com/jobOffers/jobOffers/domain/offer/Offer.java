f19c7cad4ac16d739c0fc0fb89578e3ef09d2638
yehor1959 <zaitsev.yehor@gmail.com>
2025-11-04 08:15:36 +0100

    Refactor OfferRepository

--- src/main/java/com/jobOffers/jobOffers/domain/offer/Offer.java
+++ src/main/java/com/jobOffers/jobOffers/domain/offer/Offer.java
@@ -1,15 +1,24 @@
 package com.jobOffers.jobOffers.domain.offer;
 
 import lombok.Builder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Builder
-@Document
+@Document("offers")
 record Offer(
+        @Id
         String id,
+        @Field("company")
         String companyName,
+        @Field("position")
         String position,
+        @Field("salary")
         String salary,
+        @Field("url")
+        @Indexed(unique = true)
         String offerUrl
 ) {
 }
