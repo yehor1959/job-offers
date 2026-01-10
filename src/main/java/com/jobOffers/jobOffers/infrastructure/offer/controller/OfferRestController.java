4e226b39215af889e4f6376e9e0cec69bb43bd42
yehor1959 <zaitsev.yehor@gmail.com>
2026-01-10 07:30:19 +0100

    Add saveOffer Endpoint

--- src/main/java/com/jobOffers/jobOffers/infrastructure/offer/controller/OfferRestController.java
+++ src/main/java/com/jobOffers/jobOffers/infrastructure/offer/controller/OfferRestController.java
@@ -1,12 +1,16 @@
 package com.jobOffers.jobOffers.infrastructure.offer.controller;
 
 import com.jobOffers.jobOffers.domain.offer.OfferFacade;
+import com.jobOffers.jobOffers.domain.offer.dto.OfferRequestDto;
 import com.jobOffers.jobOffers.domain.offer.dto.OfferResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +28,14 @@ public class OfferRestController {
     public ResponseEntity<List<OfferResponseDto>> findAllOffers() {
         List<OfferResponseDto> allOffers = offerFacade.findAllOffers();
         return ResponseEntity.ok(allOffers);
+    }
+
+    @PostMapping
+    public ResponseEntity<OfferResponseDto> saveOffer(@RequestBody OfferRequestDto offerRequestDto) {
+        OfferResponseDto offerResponseDto = offerFacade.saveOffer(offerRequestDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(offerResponseDto);
     }
 
     @GetMapping("/{id}")
